99bcc320e28cadb8366141c5eac673d15d022d10
Larissa Meirelles <113534712+LarissaMeirelles@users.noreply.github.com>
2023-03-03 21:41:22 -0300

    modal-perfil

---
+++ modal perfil/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+import './App.css';
+import Login from './login';
+import Perfil from './Perfil';
+import Teste from './Teste';
+
+function App() {
+
+
+  return (
+    <div>
+   <Perfil/>
+    <Teste/>
+    </div>
+  )
+}
+
+export default App
