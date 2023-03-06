a73502484504870f605929370cb8cae028dc4e8b
Ana Beatriz Castro <113535073+beatrizprog@users.noreply.github.com>
2023-03-06 09:24:53 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import './App.css'
 
 import AppRoutes from './AppRoutes'
-
+import './App.css'
+import './inicial.css'
+import Carossel from './Carossel';
+import Sobre from './Sobre';
+import Footer from './Footer';
+import Formlogin from './formlogin'
 function App() {
 
   return (
     <div className="app">
        <h1>App</h1>
        <AppRoutes />
+    <Carossel/>
+    <Sobre/>
+   <Formlogin/>
+   <Footer/>
     </div>
   )
 }
